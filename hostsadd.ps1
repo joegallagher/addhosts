f5ab479e4040d46923235f13489e49a6d5e676f9
Joe Gallagher <joegallagher95@googlemail.com>
2021-01-22 15:54:26 +0000

    Create hostsadd.ps1
    Powershell file to add entry to host file

---
+++ hostsadd.ps1
@@ -0,0 +1,17 @@
+﻿$hostName = $args[0]
+$ipAddress = $args[1]
+
+Get-PackageProvider -Name "NuGet" -Force
+
+if (Get-Module -ListAvailable -Name PsHosts) {
+    Write-Host "Module exists"
+} 
+else {
+    Install-Module PsHosts -Confirm:$False -Force 
+}
+Import-Module PsHosts
+
+If (Test-HostEntry $hostName) {
+    Remove-HostEntry $hostName
+}
+Set-HostEntry $hostName $ipAddress -force
